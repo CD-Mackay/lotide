39e96a3ed6c21ace5d217a9ba34a22d4a8a45830
CD-Mackay <connor.mackay@gmail.com>
2020-11-24 13:29:34 -0500

    are arrays equal function

---
+++ eqarrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    //console.log(`🎾🎾🎾🎾Assertion Passed: "${actual}" === "${expected}" `);
+    return true;
+  } else if (actual !== expected) {
+    //console.log(`🍎🍎🍎🍎Assertion Failed: "${actual}" !== "${expected}"`);
+    return false;
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (!assertEqual(array1[i], array2[i])) {
+      return false;
+    }
+  } return true;
+}
+
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
