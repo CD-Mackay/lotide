91063564e223f6fecf8c139ff35a3e0962b683c5
CD-Mackay <connor.mackay@gmail.com>
2020-11-25 19:03:49 +0000

    testing

--- assertobjectsequal.js
+++ assertobjectsequal.js
@@ -40,9 +40,9 @@ const eqObjects = function(object1, object2) {
 const assertObjectsEqual = function(object1, object2) {
   const inspect = require('util').inspect; 
   if (eqObjects(object1, object2)) {
-    console.log(`🟩🟩🟩🟩 Passed! ${inspect(object1)} ==== ${inspect(object2)}🟩🟩🟩🟩`);
-  } else console.log("🟥🟥🟥🟥" + object1 + "!==" + object2 + "🟥🟥🟥🟥");
+    console.log(`🟩🟩🟩🟩 Passed! ${inspect(object1)} === ${inspect(object2)}🟩🟩🟩🟩`);
+  } else console.log(`🟥🟥🟥🟥 Failed! ${inspect(object1)} !== ${inspect(object2)}🟥🟥🟥🟥`);
 };
 
-
+assertObjectsEqual({a: 1, b: 2, c: 3}, {a: 1, b: "2", c: 3});
 assertObjectsEqual({a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
