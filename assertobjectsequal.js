b22050613f123ca349b35a14b842d32388f20043
CD-Mackay <connor.mackay@gmail.com>
2020-11-25 19:02:13 +0000

    added assert function

---
+++ assertobjectsequal.js
@@ -0,0 +1,48 @@
+// assertEqual helper function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    //console.log(`🎾🎾🎾🎾Assertion Passed: "${actual}" === "${expected}" `);
+    return true;
+  } else if (actual !== expected) {
+    //console.log(`🍎🍎🍎🍎Assertion Failed: "${actual}" !== "${expected}"`);
+    return false;
+  }
+};
+
+// eqArrays helper function
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (!assertEqual(array1[i], array2[i])) {
+      return false;
+    }
+  } return true;
+};
+
+// eqObjects function
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (const element in object1) {
+    if (Array.isArray(object1[element])) {
+      if (!eqArrays(object1[element], object2[element])) {
+        return false;
+      }
+    } else if (object1[element] !== object2[element]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertObjectsEqual = function(object1, object2) {
+  const inspect = require('util').inspect; 
+  if (eqObjects(object1, object2)) {
+    console.log(`🟩🟩🟩🟩 Passed! ${inspect(object1)} ==== ${inspect(object2)}🟩🟩🟩🟩`);
+  } else console.log("🟥🟥🟥🟥" + object1 + "!==" + object2 + "🟥🟥🟥🟥");
+};
+
+
+assertObjectsEqual({a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3});
