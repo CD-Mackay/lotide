dcde271c4511e0277b71900cb02c1354c90e0940
CD-Mackay <connor.mackay@gmail.com>
2020-11-25 17:24:20 +0000

    implemented countletters function

--- countletters.js
+++ countletters.js
@@ -6,3 +6,23 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const countLetters = function(string) {
+  let result = {};
+  string = string.replace(/\s+/g, '');
+  for (const letter of string) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+
+  return result;
+}
+
+
+assertEqual(countLetters("freedom").f, 1);
+assertEqual(countLetters("freedom").e, 2);
+assertEqual(countLetters("mississippi").s, 4);
+assertEqual(countLetters("mississippi").i, 4);
+assertEqual(countLetters("mississippi").p, 2);
