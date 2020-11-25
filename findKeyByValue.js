a32b420a63d5ce74bcf912eff8828bf6eeb16301
CD-Mackay <connor.mackay@gmail.com>
2020-11-25 17:52:42 +0000

    created findkeybyvalue function

---
+++ findKeyByValue.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎾🎾🎾🎾Assertion Passed: "${actual}" === "${expected}" `);
+  } else if (actual !== expected) {
+    console.log(`🍎🍎🍎🍎Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+  
+}
