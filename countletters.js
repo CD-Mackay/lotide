221c3bccdac0aed15e16e588f32e06e53461f898
CD-Mackay <connor.mackay@gmail.com>
2020-11-25 17:16:42 +0000

    made countletters function

---
+++ countletters.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎾🎾🎾🎾Assertion Passed: "${actual}" === "${expected}" `);
+  } else if (actual !== expected) {
+    console.log(`🍎🍎🍎🍎Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
