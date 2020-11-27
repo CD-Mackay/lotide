67a5b3f9b0cbb15104159b2cbc532082e9b217ab
CD-Mackay <connor.mackay@gmail.com>
2020-11-27 21:06:14 +0000

    added recursion for nested arrays

--- eqarrays.js
+++ eqarrays.js
@@ -13,10 +13,19 @@ const eqArrays = function(array1, array2) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (!assertEqual(array1[i], array2[i])) {
+    if (Array.isArray(array1[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    }
+    else if (!assertEqual(array1[i], array2[i])) {
       return false;
     }
   } return true;
 }
 
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(assertEqual(eqArrays([1, 2, 3, [4, 5, 6, [7, 8], 9], 16],[1, 2, 3, [4, 5, 6, [7, 8], 9], 16]), true));
+console.log(assertEqual(eqArrays([1, 2, [4, 5, 6, [7, 8], 9], 16],[1, 2, 3, [4, 5, 6, [7, 8], 9], 16]), false));
+
+
