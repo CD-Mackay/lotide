f84a5b93f52ae288132aeaf809ada43123799f93
CD-Mackay <connor.mackay@gmail.com>
2020-11-30 21:52:49 +0000

    refactored to use object shorthand

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ const letterPositions = require("./letterPositions");
 const map = require("./map");
 const takeUntil = require("./takeUntil");
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  eqObjects: eqObjects,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findkey: findkey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArraysEqual,
+  assertEqual,
+  eqObjects,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  findkey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil
 };
