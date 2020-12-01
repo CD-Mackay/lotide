d0371d139c4838d83fc6e4be58ee279a9b0972d2
CD-Mackay <connor.mackay@gmail.com>
2020-12-01 00:10:29 +0000

    used recursion to make flatten more powerul

--- flatten.js
+++ flatten.js
@@ -2,13 +2,10 @@ const flatten = function(array) {
   let result = [];
   for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i])) {
-      for (let c = 0; c < array[i].length; c++) {
-        result.push(array[i][c]);
-      }
+      result = result.concat(flatten(array[i]));
     } else result.push(array[i]); 
   } return result;
 };
-
 module.exports = flatten;
 
 
